4dc88cd6ad5fd61c5cdeb2d4415b5104277cb9da
DinglePringle <sxydingle@gmail.com>
2020-03-29 19:34:08 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -22,7 +22,7 @@ client:on('ready', function()
     do
         client:setGame("with the APC Discord")
         guild = client:getGuild("679841824130859086")
-        client._specificChannel = client._PoliceGuild:getChannel("554421147328839690")
+        client._specificChannel = client._apcGuild:getChannel("554421147328839690")
     end
 end)
 
