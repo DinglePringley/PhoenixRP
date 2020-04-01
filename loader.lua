5d77f76475adce7a139bba72da9ae44e1d02b4ad
Mikesano321 <44325236+Mikesano321@users.noreply.github.com>
2020-04-02 00:13:31 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -182,7 +182,7 @@ commands[prefix.."credit"] = function(user, msg) -- Just alittle credit just use
             title = "Credit",
             fields = {
                 {name = "Bots in the APC!", value = "<@!277832237553287169>"}
-            
+          },  
             color = discordia.Color.fromRGB(255, 10, 50).value,
         }
     }    
