6c863377230d0dd0605a4633464d7e00b40fc241
DinglePringley <41307792+DinglePringley@users.noreply.github.com>
2020-04-05 03:40:39 +0100

    b

--- loader.lua
+++ loader.lua
@@ -15,7 +15,7 @@ local commands = {}
 local targets = {}
 local cooldown = {}
 local MinuteWarning = "Thursday 11:00:00"
-local Meeting = "Monday 2:10:00"
+local Meeting = "Monday 12:10:00"
 local logs = ""
 
 client:on('ready', function()
