523a77a2dc57d08829eb40f301e492a9d19b637c
DinglePringley <41307792+DinglePringley@users.noreply.github.com>
2020-04-13 04:19:57 +0100

    fdsfsdfsdf

--- loader.lua
+++ loader.lua
@@ -374,6 +374,8 @@ msg.channel:send{
 end
 	
 commands[prefix.."fultonstinks] function = (user,msg)
+	print("fulton stinks")
+end
 
 
 client:run("Bot NjkzODcxOTc5NDcxNTY4OTY4.XoSc_g.Tdvjc5_b8ggpcXUsTitX10wAJOE")
